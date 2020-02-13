e41ff1a2083c877692bc4e9ccda4ce15271c83bd
Mohamed Ashry <mohamedelashry7777@gmail.com>
2020-02-13 20:49:22 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -9,8 +9,8 @@ class Backer
   end
 
   def back_project(project)
-    # project.add_backer(self) unless project.backers.include?(self)
     @backed_projects << project
+     project.add_backer(self) unless project.backers.include?(self)
   end
 
 end
