fe21aea8845da34475472084dc6e9923b81b0e73
Mohamed Ashry <mohamedelashry7777@gmail.com>
2020-02-13 20:49:50 +0000

    Done.

---
+++ lib/project.rb
@@ -0,0 +1,16 @@
+
+
+class Project
+
+  attr_reader :title ,:backers
+  def initialize(title)
+    @title = title
+    @backers = []
+  end
+
+  def add_backer(backer)
+    # backer.back_project(self) unless backer.backed_projects.include?(self)
+    @backers << backer
+     backer.back_project(self) unless backer.backed_projects.include?(self)
+  end
+end
