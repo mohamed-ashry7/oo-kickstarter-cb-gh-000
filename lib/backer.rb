c61032f95415438033a06e56afbde7e16d389157
Mohamed Ashry <mohamedelashry7777@gmail.com>
2020-02-13 20:37:07 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -5,5 +5,9 @@ class Backer
   attr_reader :name , :backed_projects
   def initialize(name)
     @name = name
-    @backed_projects = [] 
+    @backed_projects = []
   end
+
+  def back_project
+
+end
